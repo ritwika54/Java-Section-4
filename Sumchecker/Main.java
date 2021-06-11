550d9b019fc3bec697253b8e5b9f10e5e5352ae5
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-12 00:13:31 +0530

    Create Main.java

---
+++ Sumchecker/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main (String[] args) {
+
+        System.out.println(EqualSumChecker.hasEqualSum(9, 0, 10));
+        System.out.println(EqualSumChecker.hasEqualSum(8, 4, 2));
+    }
+}
