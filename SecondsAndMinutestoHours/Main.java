03122990e500e5af092601b8dddcd31d0b32cc57
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-11 23:54:40 +0530

    Update Main.java

--- SecondsAndMinutestoHours/Main.java
+++ SecondsAndMinutestoHours/Main.java
@@ -1,13 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(SecondsAndMinutesToHours.getDurationString(81, 2));
-        System.out.println(SecondsAndMinutesToHours.getDurationString(81, 60));
-        System.out.println(SecondsAndMinutesToHours.getDurationString(-1, 50));
-        System.out.println(SecondsAndMinutesToHours.getDurationString(124, 5));
-        System.out.println(SecondsAndMinutesToHours.getDurationString(61, 0));
-
-        System.out.println(SecondsAndMinutesToHours.getDurationString(-1));
-        System.out.println(SecondsAndMinutesToHours.getDurationString(3601));
+        System.out.println(SecondsAndMinutesToHours.getDurationString(85, 9));
+        System.out.println(SecondsAndMinutesToHours.getDurationString(36,0));
+        System.out.println(SecondsAndMinutesToHours.getDurationString(3202));
     }
 }
