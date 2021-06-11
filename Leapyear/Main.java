9ad034be69736b4ca3134ed3a94324203764cdbd
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-11 23:58:00 +0530

    Create Main.java

---
+++ Leapyear/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+       
+        isLeapYear = LeapYear.isLeapYear(10_000);
+        System.out.println(isLeapYear);
+
+        isLeapYear = LeapYear.isLeapYear(3_600);
+        System.out.println(isLeapYear);
+    }
+}
