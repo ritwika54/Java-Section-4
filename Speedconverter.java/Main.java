621c94a49486011bc393f6288c0f1fb8253b7e21
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-11 23:53:32 +0530

    Create Main.java

---
+++ Speedconverter.java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(98);
+        SpeedConverter.printConversion(39.69);
+    }
+}
