54fc95d40e9958f77928943670d04ea544c7b94e
ritwika54 <58146900+ritwika54@users.noreply.github.com>
2021-06-12 00:56:38 +0530

    Create Main.java

---
+++ Teen Checker/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(TeenNumberChecker.isTeen(13));
+        System.out.println(TeenNumberChecker.hasTeen(23, 19, 13));
+        System.out.println(TeenNumberChecker.hasTeen(42, 22, 94));
+    }
+}
